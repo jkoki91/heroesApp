b8baca5cb262d26a7fe1df1c0a42f31ca52547ed
Jeff Worker <>
2022-12-13 16:24:43 +0100

    commits

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -5,7 +5,12 @@ import { HeroesService } from '../../services/heroes.service';
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+  styleUrls: ['./listado.component.css'],
+  styles:[`
+    mat-card{
+      margin-top: 20px;
+    }
+  `]
 })
 export class ListadoComponent implements OnInit {
 
