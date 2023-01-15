aeff28cfd62ef2b404ceaae7e7d923a857e5b258
Jeff Worker <>
2023-01-15 20:30:46 +0100

    heroes app finished

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth } from 'src/app/auth/interfaces/auth.interfaces';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -12,4 +15,16 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  get auth(){
+    return this.authService.auth
+  }
+
+  constructor( private authService: AuthService,
+               private router: Router){}
+
+  logout(){
+
+    this.router.navigate(['./auth']);
+
+  }
 }
